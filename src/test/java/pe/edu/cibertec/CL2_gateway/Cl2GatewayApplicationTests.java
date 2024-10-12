95e72b66c417ed735ce938518c76afc30b21f8b1
Ryuunotora <144704234+Ryuunotora@users.noreply.github.com>
2024-10-11 22:42:24 -0500

    Initial commit

---
+++ src/test/java/pe/edu/cibertec/CL2_gateway/Cl2GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL2_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
